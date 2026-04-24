db24f45aec460c83dc7bd9964615a52907db9dfd
GabrielBueno777 <gabrielbueno150809@gmail.com>
2026-04-24 01:53:31 +0000

    jogo da cobra

--- estudos.ts
+++ estudos.ts
@@ -49,30 +49,32 @@
     // const statusAtual: Status = Status.Ativo;
     // console.log(`Status atual: ${Status[statusAtual]}`);   
     
-     function somar(a: number, b: number): number {
-        return a + b;
-    }
+    //  function somar(a: number, b: number): number {
+    //     return a + b;
+    // }
 
-    console.log(`Soma: ${somar(5, 10)}`);
+    // console.log(`Soma: ${somar(5, 10)}`);
 
-    function multiplicar(a: number, b: number): number {
-        return a * b;
-    }        
+    // function multiplicar(a: number, b: number): number {
+    //     return a * b;
+    // }        
 
-    console.log(`Multiplicação: ${multiplicar(5, 10)}`);
+    // console.log(`Multiplicação: ${multiplicar(5, 10)}`);
 
-    function dividir(a: number, b: number): number {
-        if (b === 0) {
-            throw new Error("Divisão por zero não é permitida.");
-        }
-        return a / b;
-    }
+    // function dividir(a: number, b: number): number {
+    //     if (b === 0) {
+    //         throw new Error("Divisão por zero não é permitida.");
+    //     }
+    //     return a / b;
+    // }
 
-    try {
-        console.log(`Divisão: ${dividir(10, 2)}`);
-        console.log(`Divisão por zero: ${dividir(10, 0)}`);
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            console.error(error.message);
-        }
-    }
+    // try {
+    //     console.log(`Divisão: ${dividir(10, 2)}`);
+    //     console.log(`Divisão por zero: ${dividir(10, 0)}`);
+    // } catch (error: unknown) {
+    //     if (error instanceof Error) {
+    //         console.error(error.message);
+    //     }
+    // }
+
+
